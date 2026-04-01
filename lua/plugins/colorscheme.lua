90da1c84ff193c81821e789a01dc50af156cf52d
rachiepie13 <bigkitty7172@gmail.com>
2026-04-01 09:38:37 +0545

    made my neovim transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,5 +4,19 @@ return {
     opts = {
       transparent_background = true,
     },
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true, -- Enable transparency
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
   },
 }
